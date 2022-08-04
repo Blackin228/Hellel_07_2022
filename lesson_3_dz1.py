d2c3fcf48119df1de2701c5a9deb29fe8908fec8
Blackin228 <Olegsorochinskiy@gmail.com>
2022-08-04 23:37:05 +0300

    fixed lesson_3_dz1.py

--- lesson_3_dz1.py
+++ lesson_3_dz1.py
@@ -1,9 +1,43 @@
-#три однакових змінні з однаковими id:
-var_1=[50, 30, 'text']
-var_2=[50, 30, 'text']
-var_3=[50, 30, 'text']
+print("Three variables")
+print("*" * 50)
+# три однакових змінні з однаковими id:
+var_1 = 50
+var_2 = 50
+var_3 = 50
+print("var_1 = ", var_1)
+print("var_2 = ", var_2)
+print("var_3 = ", var_3)
 # перевірка:
-print(var_1==var_2==var_3)
-print(var_1 is var_2 is var_3)
+print("Check: ")
+print("var_1 == var_2 == var_3 :", var_1 == var_2 == var_3)
+print("var_1 is var_2 is var_3 :", var_1 is var_2 is var_3)
 # зміна на неоднакові id:
+var_1 = float(var_1)
+var_2 = int(var_2)
+var_3 = float(var_3)
+# перевірка:
+print("Checking changed variables: ")
+print("var_1 == var_2 == var_3 :", var_1 == var_2 == var_3)
+print("var_1 is var_2 is var_3 :", var_1 is var_2 is var_3)
+print("*" * 50)
+print("\n" * 2)
+print("Two variables")
+print("*" * 50)
+# дві змінні
+var_1 = [21, 45, 'text']
+var_2 = [21, 45, 'text']
+print("var_1 = ", var_1)
+print("var_2 = ", var_2)
+# перевірка:
+print("Check: ")
+print("var_1 == var_2 :", var_1 == var_2)
+print("var_1 is var_2 :", var_1 is var_2)
+# зміна на однакові id:
+var_1 = bool(var_1)
+var_2 = bool(var_2)
+# перевірка:
+print("Checking changed variables: ")
+print("var_1 == var_2 :", var_1 == var_2)
+print("var_1 is var_2 :", var_1 is var_2)
+print("*" * 50)
 
