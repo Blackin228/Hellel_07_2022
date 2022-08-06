bd1962425c8f951d4a5739ff44a9b9ef348df332
Blackin228 <Olegsorochinskiy@gmail.com>
2022-08-06 08:28:41 +0300

    fixed second lesson_3_dz2.py

--- lesson_3_dz2.py
+++ lesson_3_dz2.py
@@ -1,8 +1,8 @@
-input_list=input("Please enter any string:  ")
-input_list=input_list.upper()
+input_list = input("Please enter any string:  ")
+input_list = input_list.upper()
 
-first_list=list(input_list[0::2])
-second_list=list(input_list[-1::-1])
+first_list = input_list[0::2]
+second_list = input_list[-1::-1]
 
 print("Even characters from a string: ", first_list)
 print("Сharacters from the string in reverse order: ", second_list)
