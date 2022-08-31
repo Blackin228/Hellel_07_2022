aa8bdc6e3c879f1c19b31df5b85b7ca52841f70e
Blackin228 <Olegsorochinskiy@gmail.com>
2022-08-31 21:33:18 +0300

    fixed

--- Lesson11/lesson11_dz17.py
+++ Lesson11/lesson11_dz17.py
@@ -11,11 +11,10 @@ with open("dz_17.csv", mode="w", encoding="utf-8") as f:
     for key in load_dict:
         file_csv = csv.writer(f)
         if count == 0:
-            first_row = ["key", "name", "age"]
+            first_row = ["key", "name", "age", "phone_number"]
             file_csv.writerow(first_row)
             count += 1
         next_row = [key, load_dict[key][0], load_dict[key][1]]
-        print([key, load_dict[key][0], load_dict[key][1]])
         file_csv.writerow(next_row)
 
 
