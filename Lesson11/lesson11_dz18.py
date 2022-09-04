5d4a7dc6c560796b693eef0becc17393910194a6
Blackin228 <Olegsorochinskiy@gmail.com>
2022-09-04 21:28:51 +0300

    added lesson11_dz_18.py and dz_18.xlsx

--- Lesson11/lesson11_dz18.py
+++ Lesson11/lesson11_dz18.py
@@ -1,16 +1,28 @@
 import csv
 import openpyxl
 
-list_values = []
+wb = openpyxl.Workbook()
+wb.create_sheet(title="First list", index=0)
+wb.remove(wb['Sheet'])
+sheet = wb["First list"]
+list_value = []
 with open("dz_17.csv", encoding='utf-8') as file:
     file_reader = csv.reader(file, delimiter=",")
-    count = 0
-    for row in file_reader:
-        if count == 0:
-            names_column = row
-            print(f'{names_column}')
-        else:
-            list_values.append(row)
-        count += 1
-    print(list_values)
-    print(f'{count}')
+    for row in list(file_reader):
+        list_value.append([row[0], row[1], row[3]])
+
+for col_index, column in enumerate(list_value):
+    if col_index == 0:
+        continue
+    else:
+        cell = sheet.cell(row=1, column=col_index + 1)
+        cell.value = f"Person {col_index }"
+
+for col_index, column in enumerate(list_value):
+    for row_index, value in enumerate(column):
+        cell = sheet.cell(row=row_index+2, column=col_index+1)
+        cell.value = value
+
+wb.save('dz_18.xlsx')
+
+
